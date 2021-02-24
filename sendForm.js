40b94d5f3a2aa42fb25a4db0664221f64684205c
Carlos Heber <carlos.pacheco@bs2.com>
2021-02-24 12:37:41 -0300

    alteração psico form v2

---
+++ sendForm.js
@@ -0,0 +1,21 @@
+async function enviarForm(id, data, nextUrl) {
+  const formData = new FormData();
+  for (var key in data) {
+    formData.append(key, data[key]);
+  }
+
+  try {
+    const res = await fetch(
+      "https://docs.google.com/forms/d/e/" + id + "/formResponse",
+      {
+        method: "POST",
+        mode: "no-cors",
+        body: formData
+      }
+    );
+    window.location.href = nextUrl;
+  } catch (error) {
+    //console.log(error);
+    alert(error)
+  }
+}
