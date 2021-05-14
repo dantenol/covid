d5d439930b47abc23b52d0075739e984f85370ff
Dante Nolasco <dnolascodante@gmail.com>
2021-05-14 10:16:37 -0300

    fixes

--- sendForm.js
+++ sendForm.js
@@ -13,35 +13,9 @@ async function enviarForm(id, data, nextUrl) {
         body: formData
       }
     );
-   window.location.href = nextUrl;
-   console.log(res);
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-async function enviarForm2(id, data, nextUrl) {
-  const formData = new FormData();
-  for (var key in data) {
-    if(Array.isArray(data[key])){
-      data[key].forEach(function(entry) {
-        formData.append(key, entry);
-    });
-    }else{
-        formData.append(key, data[key]);
-    }
-  }
-  try {
-    const res = await fetch(
-      "https://docs.google.com/forms/d/e/" + id + "/formResponse",
-      {
-        method: "POST",
-        mode: "no-cors",
-        body: formData
-      }
-    );
     window.location.href = nextUrl;
   } catch (error) {
-    console.log(error);
+    //console.log(error);
+    alert(error)
   }
 }
